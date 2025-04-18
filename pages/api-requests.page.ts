8635e7c587a4f1fd8d396f3939a9fe0b0f1f682a
proskurko@gmail.com <proskurko@gmail.com>
2025-04-19 01:23:54 +0300

    test: update promises

--- pages/api-requests.page.ts
+++ pages/api-requests.page.ts
@@ -1,32 +1,58 @@
 import { APIRequestContext, APIResponse } from "@playwright/test";
+import {
+  iSingleUserResponse,
+  iUsersListResponse,
+} from "../types/interfaces/users.interface";
 
 export class ApiRequests {
   constructor(private request: APIRequestContext) {}
 
-  async getUserById(id: number): Promise<APIResponse> {
-    return this.request.get(`users/${id}`);
+  async getUserById(id: number): Promise<{
+    status: number;
+    body: iSingleUserResponse;
+  }> {
+    const response = await this.request.get(`users/${id}`);
+    return {
+      status: response.status(),
+      body: await response.json(),
+    };
   }
 
   async getUsersList(
     page?: number | undefined,
     perPage?: number | undefined
-  ): Promise<APIResponse> {
+  ): Promise<{
+    status: number;
+    body: iUsersListResponse;
+  }> {
+    let response: APIResponse;
+
     switch (true) {
       case page !== undefined && perPage !== undefined:
-        return this.request.get(`users?page=${2}&per_page=${3}`);
+        response = await this.request.get(
+          `users?page=${page}&per_page=${perPage}`
+        );
         break;
 
       case page !== undefined:
-        return this.request.get(`users?page=${2}`);
+        response = await this.request.get(`users?page=${page}`);
         break;
 
       case perPage !== undefined:
-        return this.request.get(`users?per_page=${3}`);
+        response = await this.request.get(`users?per_page=${perPage}`);
         break;
 
       default:
-        return this.request.get(`users`);
+        response = await this.request.get(`users`);
     }
+
+    const bodyData: iUsersListResponse = await response.json();
+
+    return { status: response.status(), body: bodyData };
+  }
+
+  async getResourceById(id: number): Promise<APIResponse> {
+    return await this.request.get(`unknown/${id}`);
   }
 }
 
