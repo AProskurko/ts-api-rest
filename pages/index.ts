428880a61e7f159be914e245c3d208c7ea216aaf
proskurko@gmail.com <proskurko@gmail.com>
2025-04-19 00:50:55 +0300

    test: add resetInstance for Singleton

--- pages/index.ts
+++ pages/index.ts
@@ -2,18 +2,23 @@ import { ApiRequests } from "./api-requests.page";
 import { APIRequestContext } from "@playwright/test";
 
 export class AppSingleton {
-    public apiRequests: ApiRequests;
+  public apiRequests: ApiRequests;
 
-    private static instance: AppSingleton;
-    private constructor(protected request: APIRequestContext) {
-        this.apiRequests = new ApiRequests(request);
-    }
+  private static instance: AppSingleton|undefined;
 
-    public static getInstance(request: APIRequestContext): AppSingleton {
-        if (!AppSingleton.instance) {
-            AppSingleton.instance = new AppSingleton(request);
-        }
-        return AppSingleton.instance;
+  private constructor(protected request: APIRequestContext) {
+    this.apiRequests = new ApiRequests(request);
+  }
+
+  public static getInstance(request: APIRequestContext): AppSingleton {
+    if (!AppSingleton.instance) {
+      AppSingleton.instance = new AppSingleton(request);
     }
+    return AppSingleton.instance;
+  }
+
+  public static resetInstance(): void {
+    AppSingleton.instance = undefined;
+  }
 }
 
