b0107ee04732b6d6d4e9059ab19e28b0feeea45c
proskurko@gmail.com <proskurko@gmail.com>
2025-04-19 00:43:13 +0300

    test: update files with JSONs

---
+++ tests/test-data/expectedResponseData.ts
@@ -0,0 +1,26 @@
+import { iSupport } from "../../interfaces/support.interface";
+import { iUser } from "../../interfaces/users.interface";
+
+const singleUserData: iUser = {
+  id: 2,
+  email: "janet.weaver@reqres.in",
+  first_name: "Janet",
+  last_name: "Weaver",
+  avatar: "https://reqres.in/img/faces/2-image.jpg",
+};
+
+const singleResourceData = {
+  id: 2,
+  name: "fuchsia rose",
+  year: 2001,
+  color: "#C74375",
+  pantone_value: "17-2031",
+};
+
+const jsonBodySupport: iSupport = {
+  url: "https://contentcaddy.io?utm_source=reqres&utm_medium=json&utm_campaign=referral",
+  text: "Tired of writing endless social media content? Let Content Caddy generate it for you.",
+};
+
+
+export { singleUserData, jsonBodySupport, singleResourceData };
