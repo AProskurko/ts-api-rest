375c17db447b825e61ae2a8c578434049b93d836
proskurko@gmail.com <proskurko@gmail.com>
2025-05-05 16:42:48 +0300

    update: base of login tests

---
+++ tests/login-logout-register.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "@playwright/test";
+import { test } from "../fixtures/base";
+import { type as t } from "arktype";
+
+//* In progress
+test.describe.serial(`Register test`, () => {
+
+})
+
+test.describe.serial(`Login test`, () => {});
