2936185a0303406f6aab440a7b8c41c1f24f0e8d
proskurko@gmail.com <proskurko@gmail.com>
2025-05-08 17:56:10 +0300

    update: add object with test data for login tests

--- tests/test-data/input-data.ts
+++ tests/test-data/input-data.ts
@@ -1,4 +1,4 @@
-const userData = {
+export const userData = {
   id: 2,
   email: "janet.weaver@reqres.in",
   first_name: "Janet",
@@ -6,3 +6,33 @@ const userData = {
   avatar: "https://reqres.in/img/faces/2-image.jpg",
 };
 
+let userEmail = "eve.holt@reqres.in";
+let userInvalidEmail = "invalidEmail";
+let userPassword = "pistol";
+
+export const loginTestData: tLoginTestData = {
+  validData: [[userEmail, userPassword, "Register user", 200]],
+  invalidData: [
+    [
+      userInvalidEmail,
+      userPassword,
+      "Register user with invalid email",
+      400,
+      "Note: Only defined users succeed registration",
+    ],
+    [userEmail, "", "Register user without password", 400, "Missing password"],
+    [
+      "",
+      userPassword,
+      "Register user without email",
+      400,
+      "Missing email or username",
+    ],
+    ["", "", "Register user without data", 400, "Missing email or username"],
+  ],
+};
+
+type tLoginTestData = {
+  validData: [string, string, string, number][];
+  invalidData: [string, string, string, number, string][];
+};
